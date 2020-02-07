37a43cb38d64490f9c764543e7b0b1a9914e87ed
jfeo <jens@feodor.dk>
2020-02-07 13:14:07 +0100

    Update elasticsearch indexing

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import sqlite3
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError as ESNotFoundError
 from neo4j import GraphDatabase, basic_auth
 from math import ceil
+import requests
 
 def index_graph_all_nodes(sqlite_db, neo4j):
     with sqlite3.connect(sqlite_db) as sqlite:
@@ -38,26 +40,40 @@ def index_graph_linked_nodes(sqlite_db, neo4j, table_name, source_id):
         sqlite.row_factory = sqlite3.Row
         c = sqlite.cursor()
 
-        props = list(map(dict, c.execute(f'SELECT DISTINCT C.* FROM {table_name} C LEFT JOIN Links L ON L.pa_id = C.pa_id AND L.source_id = {source_id} WHERE L.link_id IS NOT NULL')))
+        props = list(map(dict, c.execute(f'SELECT DISTINCT C.* FROM {table_name} C INNER JOIN Links L ON L.pa_id = C.pa_id AND L.source_id = {source_id}')))
         for prop in props:
             prop['source_id'] = source_id
         neo4j.run('UNWIND $props AS prop CREATE (pa:PersonAppearance) SET pa = prop', props=props)
 
-def index_graph_links(sqlite_db, neo4j):
+def get_links(sqlite_db):
     with sqlite3.connect(sqlite_db) as sqlite:
         sqlite.row_factory = sqlite3.Row
         c = sqlite.cursor()
-        for row in c.execute(f"SELECT L.link_id, L.score, LC.life_course_id, group_concat(L.pa_id, ',') as pa_ids, group_concat(L.source_id, ',') as source_ids FROM Links L LEFT JOIN Life_courses LC ON LC.link_id = L.link_id GROUP BY L.link_id"):
-            pa_id_a, pa_id_b = row['pa_ids'].split(',')
-            source_id_a, source_id_b = row['source_ids'].split(',')
-            life_course_id = row['life_course_id']
-            score = row['score']
-            link_id = row['link_id']
-            neo4j.run(f'''
-            MATCH (a:PersonAppearance),(b:PersonAppearance)
-            WHERE a.pa_id = {pa_id_a} AND b.pa_id = {pa_id_b} AND a.source_id = {source_id_a} AND b.source_id = {source_id_b}
-            CREATE (a)-[r:LifeLink{{link_id: {link_id}, score: {score}, life_course_id: {life_course_id} }}]->(b)
-            ''')
+        for row in c.execute(f"""
+            SELECT
+                L.link_id,
+                L.score,
+                LC.life_course_id,
+                group_concat(L.pa_id, ',') as pa_ids,
+                group_concat(L.source_id, ',') as source_ids
+            FROM Links L
+            LEFT JOIN Life_courses LC ON LC.link_id = L.link_id
+            GROUP BY L.link_id"""):
+            link = {}
+            link['pa_id_a'], link['pa_id_b'] = row['pa_ids'].split(',')
+            link['source_id_a'], link['source_id_b'] = row['source_ids'].split(',')
+            link['life_course_id'] = row['life_course_id']
+            link['score'] = row['score']
+            link['link_id'] = row['link_id']
+            yield link
+
+def index_graph_links(sqlite_db, neo4j):
+    for l in get_links(sqlite_db):
+        neo4j.run(f'''
+        MATCH (a:PersonAppearance {{ pa_id: {l["pa_id_a"]}, source_id: {l["source_id_a"]} }})
+        MATCH (b:PersonAppearance {{ pa_id: {l["pa_id_b"]}, source_id: {l["source_id_b"]} }})
+        CREATE (a)-[ll:LifeLink{{ link_id: {l["link_id"]}, life_course_id: {l["life_course_id"]}, score: {l["score"]}}}]->(b)
+        ''').consume()
 
 
 def index_nested(sqlite_db, es):
@@ -68,20 +84,39 @@ def index_nested(sqlite_db, es):
         sqlite.row_factory = sqlite3.Row
         c = sqlite.cursor()
         last_life_course_id = None
-        life_course = []
+        last_link_id = None
+        life_course = {}
+        link = []
         for row in c.execute(query):
+            index_nested_pa(row)
             if life_course and last_life_course_id != row['life_course_id']:
                 index_nested_lifecourse(life_course)
-                life_course = []
-            life_course.append(row)
+                life_course = {}
+            if link and last_link_id != row['link_id']:
+                index_nested_link(link)
+                link = []
+            life_course[row['pa_id']] = row
+            link.append(row)
             last_life_course_id = row['life_course_id']
-            
+            last_link_id = row['link_id']
+
+def index_nested_pa(pa):
+    doc = {
+        "personal_appearance": {key: pa[key] for key in dict(pa) if pa[key] != ""}
+    }
+    es.index(index="pas", id=f"{pa['source_id']}-{pa['pa_id']}", body=doc)
+
+def index_nested_link(link):
+    doc = {
+        "personal_appearance": list(map(lambda row: {key: row[key] for key in dict(row) if row[key] != ""}, link))
+    }
+    es.index(index="links", id=link[0]['link_id'], body=doc)
 
 def index_nested_lifecourse(life_course):
-    life_course = {
-        "pa": list(map(lambda row: {key: row[key] for key in dict(row) if row[key] != ""}, life_course))
+    doc = {
+        "personal_appearance": list(map(lambda row: {key: row[key] for key in dict(row) if row[key] != ""}, life_course.values()))
     }
-    es.index(index='link-lives', id=life_course['pa'][0]['life_course_id'], body=life_course)
+    es.index(index='lifecourses', id=list(life_course.values())[0]['life_course_id'], body=doc)
 
 def index_flat(sqlite_db, es):
     with open('lifecourses_full_sorted.sql', 'r', encoding='utf-8') as sqlfile:
@@ -93,20 +128,122 @@ def index_flat(sqlite_db, es):
         for row in c.execute(query):
             es.index(index='link-lives', id=f"{row['life_course_id']}-{row['pa_id']}-{row['source_id']}", body=dict(row))
 
+def es_mapping_pa_props():
+    return {
+        "firstnames": {"type": "text"},
+        "surnames": {"type": "text"},
+        "familyname": {"type": "text"},
+        "firstnames_std": {"type": "keyword"},
+        "surnames_std": {"type": "keyword"},
+        "maidenname_std": {"type": "keyword"},
+        "familyname_std": {"type": "keyword"},
+        "sex": {"type": "keyword"},
+        "birthplace": {"type": "text"},
+        "birthdate": {"type": "date"},
+        "deathdate": {"type": "date"},
+        "county": {"type": "text"},
+        "district": {"type": "text"},
+        "parish": {"type": "text"},
+        "county_std": {"type": "keyword"},
+        "district_std": {"type": "keyword"},
+        "parish_std": {"type": "keyword"}
+    }
+
+def es_mapping_pa():
+    return {
+        "type": "nested",
+        "properties": {
+            "link_id": {"type": "integer"},
+            "lifecourse_id": {"type": "integer"},
+
+            "firstnames": {"type": "text"},
+            "surnames": {"type": "text"},
+            "familyname": {"type": "text"},
+            "firstnames_std": {"type": "keyword"},
+            "surnames_std": {"type": "keyword"},
+            "maidenname_std": {"type": "keyword"},
+            "familyname_std": {"type": "keyword"},
+            "sex": {"type": "keyword"},
+            "birthplace": {"type": "text"},
+            "birthdate": {"type": "date"},
+            "deathdate": {"type": "date"},
+            "county": {"type": "text"},
+            "district": {"type": "text"},
+            "parish": {"type": "text"},
+            "county_std": {"type": "keyword"},
+            "district_std": {"type": "keyword"},
+            "parish_std": {"type": "keyword"},
+        }
+    }
+
+def es_mappings_index_lifecourses():
+    return {
+        "dynamic": False,
+        "properties": {
+            "id": {"type": "integer"},
+            "personal_appearance": es_mapping_pa()
+        }
+    }
+
+def es_mappings_index_links():
+    return {
+        "dynamic": False,
+        "properties": {
+            "id": {"type": "integer"},
+            "personal_appearance": es_mapping_pa(),
+            "method": {"type": "keyword"},
+            "score": {"type": "float"}
+        }
+    }
+
+def es_mappings_index_pas():
+    return {
+        "dynamic": False,
+        "properties": {
+            "id": {"type": "integer"},
+            "personal_appearance": es_mapping_pa()
+        }
+    }
 
 
 if __name__ == "__main__":
     import sys
     if sys.argv[1] == 'es':
-        es = Elasticsearch()
-        index_nested('LL.db', es)
+        es = Elasticsearch(hosts=["localhost:9200", "localhost:9300"])
+        if len(sys.argv) == 3 and sys.argv[2] == "setup":
+            print("deleting indices")
+            try:
+                es.indices.delete("links,lifecourses,pas")
+            except:
+                pass
+
+            print("setting up indices")
+            print(" => creating links index")
+            es.indices.create('links')
+            print(" => putting links mapping")
+            es.indices.put_mapping(index='links', body=es_mappings_index_links())
+
+            print(" => creating lifecourses index")
+            es.indices.create('lifecourses')   
+            print(" => putting lifecourse mapping")
+            es.indices.put_mapping(index='lifecourses', body=es_mappings_index_lifecourses())         
+
+            print(" => creating pas index")
+            es.indices.create('pas')
+            print(" => putting pas mapping")
+            es.indices.put_mapping(index='pas', body=es_mappings_index_pas())
+        else:
+            index_nested('LL.db', es)
     elif sys.argv[1] == 'graph':
         driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth('neo4j', '123456'))
         session = driver.session()
         if sys.argv[2] == "nodes":
             print("indexing nodes")
-            index_graph_linked_nodes('LL.db', session, "census_1860", 0)
-            index_graph_linked_nodes('LL.db', session, "census_1850", 1)
+            print('census 1860')
+            #index_graph_linked_nodes('LL.db', session, "census_1860", 0)
+            print('census 1850')
+            #index_graph_linked_nodes('LL.db', session, "census_1850", 1)
+            print('census 1845')
             index_graph_linked_nodes('LL.db', session, "census_1845", 2)
         elif sys.argv[2] == "links":
             print("indexing links")
--- lifecourses_full_sorted.sql
+++ lifecourses_full_sorted.sql
@@ -1,20 +1,20 @@
 SELECT
 	LC.life_course_id,
 	L.*,
-	coalesce(C45.navn, '') || coalesce(C50.navn, '') || coalesce(C60.navn, '') as navn,
-	coalesce(C45.køn, '') || coalesce(C50.køn, '') || coalesce(C60.køn, '') as køn,
-	coalesce(C45.alder, '') || coalesce(C50.alder, '') || coalesce(C60.alder, '') as alder,
-	coalesce(C45.fødested, '') || coalesce(C50.fødested, '') || coalesce(C60.fødested, '') as fødested,
-	coalesce(C45.Sogne, '') || coalesce(C50.Sogne, '') || coalesce(C60.Sogne, '') as Sogne,
-	coalesce(C45.Amt, '') || coalesce(C50.Amt, '') || coalesce(C60.Amt, '') as Amt,
-	coalesce(C45.Herred, '') || coalesce(C50.Herred, '') || coalesce(C60.Herred, '') as Herred,
+	coalesce(C45.navn, '') || coalesce(C50.navn, '') || coalesce(C60.navn, '') as name,
+	coalesce(C45.køn, '') || coalesce(C50.køn, '') || coalesce(C60.køn, '') as sex,
+	coalesce(C45.alder, '') || coalesce(C50.alder, '') || coalesce(C60.alder, '') as age,
+	coalesce(C45.fødested, '') || coalesce(C50.fødested, '') || coalesce(C60.fødested, '') as birthplace,
+	coalesce(C45.Sogne, '') || coalesce(C50.Sogne, '') || coalesce(C60.Sogne, '') as parish,
+	coalesce(C45.Amt, '') || coalesce(C50.Amt, '') || coalesce(C60.Amt, '') as county,
+	coalesce(C45.Herred, '') || coalesce(C50.Herred, '') || coalesce(C60.Herred, '') as district,
 	coalesce(C45.name_clean, '') || coalesce(C50.name_clean, '') || coalesce(C60.name_clean, '') as name_clean,
 	coalesce(C45.age_clean, '') || coalesce(C50.age_clean, '') || coalesce(C60.age_clean, '') as age_clean,
-	coalesce(C45.gender_clean, '') || coalesce(C50.gender_clean, '') || coalesce(C60.gender_clean, '') as gender_clean,
+	coalesce(C45.gender_clean, '') || coalesce(C50.gender_clean, '') || coalesce(C60.gender_clean, '') as sex_clean,
 	coalesce(C45.birth_place_clean, '') || coalesce(C50.birth_place_clean, '') || coalesce(C60.birth_place_clean, '') as birth_place_clean,
 	coalesce(C45.name_std, '') || coalesce(C50.name_std, '') || coalesce(C60.name_std, '') as name_std,
-	coalesce(C45.first_name_std, '') || coalesce(C50.first_name_std, '') || coalesce(C60.first_name_std, '') as first_name_std,
-	coalesce(C45.last_name_std, '') || coalesce(C50.last_name_std, '') || coalesce(C60.last_name_std, '') as last_name_std,
+	coalesce(C45.first_name_std, '') || coalesce(C50.first_name_std, '') || coalesce(C60.first_name_std, '') as firstnames_std,
+	coalesce(C45.last_name_std, '') || coalesce(C50.last_name_std, '') || coalesce(C60.last_name_std, '') as surnames_std,
 	coalesce(C45.county_std, '') || coalesce(C50.county_std, '') || coalesce(C60.county_std, '') as county_std,
 	coalesce(C45.parish_std, '') || coalesce(C50.parish_std, '') || coalesce(C60.parish_std, '') as parish_std,
 	coalesce(C45.other_std, '') || coalesce(C50.other_std, '') || coalesce(C60.other_std, '') as other_std,
@@ -24,4 +24,4 @@ LEFT JOIN Life_courses LC ON L.link_id = LC.link_id
 LEFT JOIN census_1845 C45 ON C45.pa_id = L.pa_id AND L.source_id = 2
 LEFT JOIN census_1850 C50 ON C50.pa_id = L.pa_id AND L.source_id = 1
 LEFT JOIN census_1860 C60 ON C60.pa_id = L.pa_id AND L.source_id = 0
-ORDER BY LC.life_course_id;
+ORDER BY LC.life_course_id, L.link_id;
